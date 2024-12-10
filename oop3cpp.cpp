b638d34f35994a606e6a583245c7eee4e2988eb2
Ujjal Mahata <ujjadsingh66@gmail.com>
2024-12-10 11:53:09 +0545

    all code

---
+++ oop3cpp.cpp
@@ -0,0 +1,37 @@
+#include<iostream.h>
+using name space std;
+class student 
+{
+	privite:int roll;
+	float marks;
+	char name[];
+	public:void input();
+	int x;
+	void enter()
+	{
+		cout<<"input data"<<endl;
+		cin>>name>>rolls>>marks>>x;
+		
+	}
+	void display()
+	{
+		cout<<name<<"\t"<<rolls<<"\t"marks<<"t"<<X<<endl;
+	}
+};
+void student::input()
+{
+	cout<<"ujjal is king"<<endl;
+	
+}
+int main()
+{
+	student s1,s2,s3;
+	s2.x=9;
+	s2.input();
+	s3.input();
+	s2.inter();
+	s3.inter();
+	s2.display();
+	s3.display();
+	return 0;
+	}
