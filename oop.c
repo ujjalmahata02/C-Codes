8f922f20c24f04652b62e859a49277d3b4277f5f
Ujjal Mahata <ujjadsingh66@gmail.com>
2024-12-10 11:50:25 +0545

    gngjn

---
+++ oop.c
@@ -0,0 +1,46 @@
+#include<iostream.h>
+using name space std;
+class student
+ {
+  int roll;
+  char name[20];
+  public:void enter()
+
+   {
+    cin>>name>>roll;
+       
+   }
+   void display()
+   {
+    cout<<name<<roll;
+   }
+   class date
+   {
+    int day,month,year;
+    public:void get data()
+           void show data()
+   };
+   };
+   void student::date::get data()
+   {
+    cout<<"enter date"<<endl;
+    cin>>day month year;
+  
+   }
+   void student::date::show date ()
+   {
+    cout<<year<<month<<day;
+
+   }
+   int main()
+   {
+    student::date dl;
+    s1.enter();
+    d1.get date();
+    s1.display();
+    d1.show date();
+    return 0;
+   }
+
+    return 0;
+ }
