b312b1c1753e90aa64aaf53625b65bbbd220a82f
Ujjal Mahata <ujjadsingh66@gmail.com>
2025-01-23 16:47:45 +0545

    commit

--- ujjal.c
+++ ujjal.c
@@ -1,18 +1,5 @@
-#include<stdio.h>
 
-int add(int, int);
 
-int main() {
-    int a, b, sum;
-    printf("Enter two integers: ");
-    scanf("%d%d", &a, &b);
-    sum = add(a, b);
-    printf("\nSum = %d", sum);
-    return 0;
-}
 
-int add(int p, int q) {
-    int t;
-    t = p + q;
-    return t;
-}
+#include<conio.h>
+void main();
